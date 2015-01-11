1746fa62a4cd4952b51d2ac3866db812c3aee872
Kamil Adamczyk <kadamczyk@MacBook-Air-Kamil.local>
2015-01-11 14:53:02 +0100

    wyświetlanie waluty

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ActiveRecord::Base
-
+  
   validates :car_brand, presence: true,
                         length: { in: 3..30 }
   validates :car_model, presence: true,
@@ -7,7 +7,7 @@ class Car < ActiveRecord::Base
   validates :fuel_type, presence: true,
                         length: { in: 1..30 }
   validates :num_of_seats,  presence: true,
-                            length: { in: 3..7 }
+                            numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 7 }
   validates :price_per_day, presence: true
 
 end
